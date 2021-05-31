a96b0f1cabb46315ee7aa7e4d7580ae390f86b0f
hebert <hebertvillarreal1@gmail.com>
2021-05-31 16:54:40 -0600

    Se quita el código comentado y se colocan las constantes en un solo lugar

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const typedText = document.querySelector('.typed-text');
 const cursorSpan = document.querySelector('.cursor');
+const userName = document.getElementById("name");
+const userEmail = document.getElementById('email');
+const userPhone = document.getElementById('phone');
+const userAsunto = document.getElementById('asunto');
+const userMessage = document.getElementById('message');
+const formulario = document.getElementById('formulario');
 const textArray = ['JavaScript', 'HTML', 'CSS', 'Node.js', 'Java', 'Python', 'Django Rest Framework'];
 const typingDelay = 100;
 const erasingDelay = 70;
@@ -48,15 +54,8 @@ btnSwitch.addEventListener('click', () => {
     btnSwitch.classList.toggle('active')
 });
 
-//Validar el form
-const userName = document.getElementById("name");
-const userEmail = document.getElementById('email');
-const userPhone = document.getElementById('phone');
-const userAsunto = document.getElementById('asunto');
-const userMessage = document.getElementById('message');
-const formulario = document.getElementById('formulario');
 
-
+// Validar y enviar el form a netlify
 formulario.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -109,46 +108,3 @@ formulario.addEventListener('submit', e => {
         }
     });
 });
-
-// document.querySelector("form").addEventListener("submit", handleSubmit);
-// const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     let myForm = document.getElementById("formulario");
-//     let formData = new formulario(myForm)
-//     fetch('/', {
-//         method: 'POST',
-//         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//         body: new URLSearchParams(formData).toString()
-//     }).then(() => console.log('Form successfully submitted')).catch((error) =>
-//         alert(error))
-
-// }
-
-// formulario.addEventListener("submit", event => {
-//     event.preventDefault();
-//     console.log('Hola');
-//     let regExpEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$|^$/
-//     let regExpName = /^[A-Za-z]*$/
-//     let regExpPhone = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$|^$/g
-
-
-//     if(userName.value == '') {
-//         alert('Debe de ingresar su nombre');
-//     }
-//     if(userEmail.value == '' && userPhone.value == '') {
-//         alert('Debe de ingresar al menos un correo electrónico o un teléfono para poder contactarte');
-//     }
-//     if(userMessage.value == '') {
-//         alert('Debe de ingresar algún mensaje');
-//     }
-//     if(!regExpName.test(userName.value)) {
-//         alert('Su nombre no debe de contener caracteres especiales');
-//     }
-//     if(!regExpEmail.test(userEmail.value)) {
-//         alert('Su correo electrónico no cuenta con el formato correcto');
-//     }
-//     if(!regExpPhone.test(userPhone.value)) {
-//         alert('Su teléfono no cuenta con el formato correcto');
-//     }
-// })
