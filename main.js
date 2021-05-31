be4fd6553469ae02d959477caff2d90e4c3e3875
hebert <hebertvillarreal1@gmail.com>
2021-05-31 15:55:07 -0600

    Se realiza AJAX para el envío de formularios con netlify

--- main.js
+++ main.js
@@ -54,12 +54,28 @@ const userEmail = document.getElementById('email');
 const userPhone = document.getElementById('phone');
 const userAsunto = document.getElementById('asunto');
 const userMessage = document.getElementById('message');
-const form = document.getElementById('form');
+const formulario = document.getElementById('formulario');
 
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+document.querySelector("form").addEventListener("submit", handleSubmit);
+validation();
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let myForm = document.getElementById("formulario");
+    let formData = new formulario(myForm)
+    fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+        alert(error))
+
+}
+
+function validation() {
     
+    console.log('Hola');
     let regExpEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$|^$/
     let regExpName = /^[A-Za-z]*$/
     let regExpPhone = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$|^$/g
@@ -83,4 +99,33 @@ form.addEventListener("submit", (event) => {
     if(!regExpPhone.test(userPhone.value)) {
         alert('Su teléfono no cuenta con el formato correcto');
     }
-})
+
+}
+
+// formulario.addEventListener("submit", event => {
+//     event.preventDefault();
+//     console.log('Hola');
+//     let regExpEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$|^$/
+//     let regExpName = /^[A-Za-z]*$/
+//     let regExpPhone = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$|^$/g
+
+
+//     if(userName.value == '') {
+//         alert('Debe de ingresar su nombre');
+//     }
+//     if(userEmail.value == '' && userPhone.value == '') {
+//         alert('Debe de ingresar al menos un correo electrónico o un teléfono para poder contactarte');
+//     }
+//     if(userMessage.value == '') {
+//         alert('Debe de ingresar algún mensaje');
+//     }
+//     if(!regExpName.test(userName.value)) {
+//         alert('Su nombre no debe de contener caracteres especiales');
+//     }
+//     if(!regExpEmail.test(userEmail.value)) {
+//         alert('Su correo electrónico no cuenta con el formato correcto');
+//     }
+//     if(!regExpPhone.test(userPhone.value)) {
+//         alert('Su teléfono no cuenta con el formato correcto');
+//     }
+// })
